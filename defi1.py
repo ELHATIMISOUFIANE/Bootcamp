ee120c611acf9a7f49a8ae5a4b53b5017ed5f98d
ELHATIMISOUFIANE <soufianeelhatimi@gmail.com>
2025-03-10 17:25:34 +0100

    Defi1

---
+++ defi1.py
@@ -0,0 +1,9 @@
+
+number = int(input("Entrez un nombre : "))
+length = int(input("Entrez la longueur de la liste : "))
+
+
+multiples = [number * i for i in range(1, length + 1)]
+
+
+print("Liste des multiples :", multiples)
