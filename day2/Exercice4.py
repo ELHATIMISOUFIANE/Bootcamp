d8c8ca44861fd64d703cbb5635fcab93cbc1e2d3
ELHATIMISOUFIANE <soufianeelhatimi@gmail.com>
2025-03-12 00:18:19 +0100

    Ex4

---
+++ day2/Exercice4.py
@@ -0,0 +1,8 @@
+def describe_city(city, country="France"):
+    print(f"{city} est en {country}.")
+
+
+
+describe_city("Paris")
+describe_city("Berlin", "Allemagne")
+describe_city("New York", "États-Unis")
