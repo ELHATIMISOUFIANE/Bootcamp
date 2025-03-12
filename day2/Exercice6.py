52787746e922278414365ad3f630b41d1cd33cdb
ELHATIMISOUFIANE <soufianeelhatimi@gmail.com>
2025-03-12 01:16:29 +0100

    Ex6

---
+++ day2/Exercice6.py
@@ -0,0 +1,8 @@
+def make_shirt(size="L", text="J'aime Python"):
+    print(f"La taille de la chemise est {size} et le texte est : '{text}'.")
+
+
+make_shirt()
+make_shirt(size="M")
+make_shirt(size="S", text="coucou !")  #
+make_shirt(size="XL", text="Je code en Python !")
