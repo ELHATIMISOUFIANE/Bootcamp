c275bd2a32bc17d345d5facaf7ba734195229884
ELHATIMISOUFIANE <soufianeelhatimi@gmail.com>
2025-03-14 16:17:55 +0100

    Ex1

--- day4/Exercice1.py
+++ day4/Exercice1.py
@@ -1,38 +1,43 @@
-class Dog:
-    def __init__(self, name, age, weight):
+class Pets():
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+class Cat():
+    is_lazy = True
+
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.weight = weight
 
-    def bark(self):
-        return f"{self.name} est aboying"
+    def walk(self):
+        return f'{self.name} is just walking around'
 
-    def run_speed(self):
-        return self.weight / self.age * 10
+class Bengal(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
 
-    def fight(self, other_dog):
-        self_power = self.run_speed() * self.weight
-        other_power = other_dog.run_speed() * other_dog.weight
+class Chartreux(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
 
-        if self_power > other_power:
-            return f"{self.name} a gagné le combat contre {other_dog.name} !"
-        elif self_power < other_power:
-            return f"{other_dog.name} a gagné le combat contre {self.name} !"
-        else:
-            return f"Match nul entre {self.name} et {other_dog.name} !"
+# Créer une nouvelle classe Siamese qui hérite de Cat
+class Siamese(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
 
+# Créer une liste all_cats contenant trois instances de chats
+all_cats = [
+    Bengal("Bengie", 3),
+    Chartreux("Charlie", 5),
+    Siamese("Simba", 2)
+]
 
-# Créer 3 chiens
-dog1 = Dog("Rex", 5, 30)
-dog2 = Dog("Max", 3, 25)
-dog3 = Dog("Buddy", 7, 40)
+# Créer une instance de Pets avec la liste all_cats
+sara_pets = Pets(all_cats)
 
-# Faire aboyer les chiens
-print(dog1.bark())  # Rex est aboying
-print(dog2.bark())  # Max est aboying
-print(dog3.bark())  # Buddy est aboying
-
-# Faire combattre les chiens
-print(dog1.fight(dog2))  # Rex a gagné le combat contre Max !
-print(dog2.fight(dog3))  # Buddy a gagné le combat contre Max !
-print(dog3.fight(dog1))  # Buddy a gagné le combat contre Rex !
+# Faire promener tous les chats
+sara_pets.walk()
